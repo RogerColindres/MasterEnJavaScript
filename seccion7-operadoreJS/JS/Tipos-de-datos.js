353f4b2ef74f8fd1277bde3debdf58449e871ec8
RogerColindres <jeovannyr10@gmail.com>
2020-03-05 17:34:07 -0600

    Tipos de datos y operadores en js

---
+++ seccion7-operadoreJS/JS/Tipos-de-datos.js
@@ -0,0 +1,23 @@
+//operadores
+var numero = 7;
+var numero2 = 12;
+var operacion = (numero * numero2);
+//alert("El resultado de la operacion es: " + operacion);
+
+
+//tipos de datos
+var numeroentero = 44;
+var cadenatexto = "hola mundo"
+var booleano = true; //or false
+
+//funciones para convertir un dato a otro
+var numero_falso = "33";
+console.log(numero_falso + 7);
+console.log(Number(numero_falso) + 7);
+
+console.log(String(numeroentero) + 7);
+
+//detectar el tipo de dato 
+//funcion typeof para detectar el tipo de dato de la variable
+console.log(typeof(numeroentero));
+console.log(typeof(cadenatexto));
