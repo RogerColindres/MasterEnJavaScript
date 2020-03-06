c026bf669f9f9e9ee9651ada575f903d1b3fdf6f
RogerColindres <jeovannyr10@gmail.com>
2020-03-06 10:53:54 -0600

    Alertas y tipos de ventanas emergentes en JS

---
+++ AlertasyVentanas.js/JS/ventanas.js
@@ -0,0 +1,10 @@
+//alertas
+alert("Bienvenido a nuestro blog");
+
+//alertas de confirmacion
+var mi_resultado = confirm("esta seguro que desea continuar");
+console.log(mi_resultado);
+
+//ventana de ingreso de informacion
+var mi_resultado2 = prompt("¿Como estas?", "bien");
+console.log(mi_resultado2);
